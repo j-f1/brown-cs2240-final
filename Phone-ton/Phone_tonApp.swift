dac6a669a8fe50981b0baab144f2e0e1b78d9277
Jed Fox <git@jedfox.com>
2023-04-02 14:49:13 -0400

    Initial Commit

---
+++ Phone-ton/Phone_tonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phone_tonApp.swift
+//  Phone-ton
+//
+//  Created by Jed Fox on 2023-04-02.
+//
+
+import SwiftUI
+
+@main
+struct Phone_tonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
